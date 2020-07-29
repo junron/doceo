56eec6ef55478f367817dd11c6d4f0ee11d05ee6
JavierLimZH <32543880+JavierLimZH@users.noreply.github.com>
2020-07-29 20:13:37 +0800

    submit

--- app/src/main/java/com/example/attendance/fragments/snapmit/submit/LoadingFragment.java
+++ app/src/main/java/com/example/attendance/fragments/snapmit/submit/LoadingFragment.java
@@ -1,22 +1,40 @@
 package com.example.attendance.fragments.snapmit.submit;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.attendance.R;
+import com.example.attendance.util.android.SafeLiveData;
 import com.example.attendance.viewmodels.SubmitViewModel;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -62,7 +80,55 @@ public class LoadingFragment extends Fragment {
             if (imageURLs.size() == 0) return;
             if (Thread.interrupted()) return;
 
-            setLoadingText("Sending to Firebase", loadingText);
+            setLoadingText("Submitting", loadingText);
+
+            Map<String, Object> data = new HashMap<>();
+            String assID = submitViewModel.getAssignmentUUID().getValue();
+            data.put("images", imageURLs);
+            data.put("assignmentId", assID);
+            data.put("comment", "");
+            data.put("owner", FirebaseAuth.getInstance().getCurrentUser().getEmail());
+            data.put("submissionTime", Timestamp.now());
+            data.put("deleted", false);
+
+
+            Task<DocumentReference> upload = FirebaseFirestore.getInstance().collection("submissions").add(data);
+            Task<Void> up = upload.continueWithTask(task -> {
+                DocumentReference doc = task.getResult();
+                doc.update("id", doc.getId());
+                DocumentSnapshot docSubmit = FirebaseFirestore.getInstance().document("assignments/"+assID).get().getResult();
+                List<String> submissions = (List<String>) docSubmit.get("submissions");
+                submissions.add(doc.getId());
+                return docSubmit.getReference().update("submissions", submissions);
+            });
+
+            up.addOnSuccessListener(d -> {
+                if (fragment.getActivity() == null) return;
+                view.findViewById(R.id.loading).animate().alpha(0);
+                new MaterialAlertDialogBuilder(fragment.getContext(), R.style.SuccessDialog)
+                        .setTitle("Success!")
+                        .setMessage("Your assignment was handed in successfully!")
+                        .setIcon(R.drawable.ic_check_black_24dp)
+                        .setOnCancelListener(dialog -> {
+                            NavHostFragment.findNavController(this).navigateUp();
+                            NavHostFragment.findNavController(this).navigateUp();
+                            submitViewModel.setImagesData(new SafeLiveData<>(new ArrayList<>()));
+                            view.findViewById(R.id.loading).setAlpha(1);
+                        })
+                        .show();
+                });
+            up.addOnFailureListener(e -> {
+                Log.e("UPLOAD", e.toString());
+                if (fragment.getActivity() == null) return;
+                new MaterialAlertDialogBuilder(fragment.getContext(), R.style.ErrorDialog)
+                        .setTitle("An error occured.")
+                        .setIcon(R.drawable.ic_error_outline_black_24dp)
+                        .setMessage(e.getMessage())
+                        .setOnDismissListener(dialog -> {
+                            NavHostFragment.findNavController(this).navigateUp();
+                        }).show();
+                setLoadingText(e.getMessage(), loadingText);
+            });
 
         });
         loadingThread.start();
@@ -77,21 +143,7 @@ public class LoadingFragment extends Fragment {
     }
 
     private ArrayList<String> uploadImages() {
-        ExecutorService executorService = Executors.newFixedThreadPool(10   );
-        ArrayList<Future<JsonElement>> uploadTasks = new ArrayList<>();
-
-
-        ArrayList<String> titles = new ArrayList<>();
-        for (Future<JsonElement> future : uploadTasks) {
-            try {
-                JsonObject obj = (JsonObject) future.get();
-                titles.add(obj.get("data").getAsJsonObject().get("link").getAsString());
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return titles;
+        return (ArrayList<String>) UploaderKt.upload(images);
     }
 
     void setLoadingText(String s, TextView loadingTextView) {
--- app/src/main/java/com/example/attendance/fragments/snapmit/submit/Uploader.kt
+++ app/src/main/java/com/example/attendance/fragments/snapmit/submit/Uploader.kt
@@ -7,18 +7,15 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.File
 
-class Uploader {
-    companion object {
-        fun upload(files: List<File>): List<String> {
-            var urls: List<String> = listOf()
-            GlobalScope.launch(Dispatchers.IO) {
-                urls = files.map {
-                    CloudStorage.addObject(it)
-                }
-            }
-            while (urls.isEmpty()) {}
-            Log.d("UPLOAD", urls[0])
-            return urls
+
+fun upload(files: List<File>): List<String> {
+    var urls: List<String> = listOf()
+    GlobalScope.launch(Dispatchers.IO) {
+        urls = files.map {
+            CloudStorage.addObject(it)
         }
     }
+    while (urls.isEmpty()) {}
+    Log.d("UPLOAD", urls[0])
+    return urls
 }
